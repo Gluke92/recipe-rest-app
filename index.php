c8556c175b8a375f116ff4a02badf4550ae1fe1d
George Luke <george.luke92@gmail.com>
2019-07-31 16:34:38 -0500

    massive changes to validator system

--- index.php
+++ index.php
@@ -12,11 +12,31 @@
         <link rel="stylesheet" type="text/css" href="../main.css">
     </head>
     <body>
-        <div>WHAT</div>
+        <div>Login Here</div>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+                <button type="submit">Register</button>
+            </form>
+        </div>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         
+        <div class="container">
+            <div class="row">
+                <div class="col-d-4 offset-md-4"></div>
+                    <form action="signup.php" method="post"></form>
+                        <h3 class="text-center">Register</h3>
+
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            input[name-username].form-control.form-control
+                        </div>
+            </div>
+        </div>
         <script src="" async defer></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </body>
 </html>
