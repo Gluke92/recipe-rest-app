0d5992d5e20b4ee7217f3f5aa2e62287385ed37d
George Luke <george.luke92@gmail.com>
2019-08-01 13:38:46 -0500

    new home

---
+++ Authenticator/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>User registration system with PHP and mySQL</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../main.css">
+    </head>
+    <body>
+        <div>Login Here</div>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+                <button type="submit">Register</button>
+            </form>
+        </div>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-d-4 offset-md-4"></div>
+                    <form action="signup.php" method="post"></form>
+                        <h3 class="text-center">Register</h3>
+
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            input[name-username].form-control.form-control
+                        </div>
+            </div>
+        </div>
+        <script src="" async defer></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </body>
+</html>
