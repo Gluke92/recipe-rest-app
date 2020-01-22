09eeb2a8c48826c5fdbc8469b366525e09447633
George Luke <george.luke92@gmail.com>
2020-01-22 12:54:55 -0600

    added links, navbar, styling, and updated routes

---
+++ react-frontend-01-starting-setup/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/UIElements/Navigation/MainNavigation.jsx";
+// set up alternative routes. Switch from react-router-dom will wrap the react components,
+//and prevent switching.
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>{" "}
+      </main>
+    </Router>
+  );
+};
+
+export default App;
